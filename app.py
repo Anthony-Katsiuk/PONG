747d8a944d791794bf3e7cf31d533c104c114ea2
ANTMAN178 <85637059+Anthony-Katsiuk@users.noreply.github.com>
2023-06-09 12:36:17 -0400

    Update app.py
    changes icon.png to pong_icon.png

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((800, 500))
 pygame.display.set_caption("PONG")
-icon = pygame.image.load('graphics/icon.png')
+icon = pygame.image.load('graphics/pong_icon.png')
 pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
 
